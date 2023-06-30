f254085e09fe2ab8007d1b6fa835f87add2dbecc
Kavinhbn <132799226+Kavinhbn@users.noreply.github.com>
2023-06-30 11:02:15 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,140 @@
+const logotext = "KAVIN-HBN";
+const meta = {
+    title: "Kavin HBN",
+    description: "I’m Kavin HBN data ,currently learning React to continue with React native",
+};
+
+const introdata = {
+    title: "I’m Kavin HBN",
+    animated: {
+        first: "I love coding",
+        second: "I code some websites",
+        third: "I love to hear songs",
+    },
+    description: "",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: " about my self",
+    aboutme: "Working alone guy, love to hear favourite playlist.",
+};
+const worktimeline = [{
+}
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    
+];
+
+const services = [{
+        title: "Web Design",
+        description: "As a fresh and enthusiastic web design developer, I bring a unique innovation in it,and I will ensure that the website gonna stand out from crowd with it's design . Let's embark on this digital journey and leave out a everlasting digital presence.  ",
+    },
+    
+    
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "KavinHBN@gmail.com",
+    YOUR_PHONE: "SOMEDAY",
+    description: "As a fresh web design developer i have some trust issues. please contact me through gmail.",
+   
+    YOUR_SERVICE_ID: "service_rdi2cih",
+    YOUR_TEMPLATE_ID: "template_0ilj7jg",
+    YOUR_USER_ID: "vlOcS_1KHAplTkObl",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
